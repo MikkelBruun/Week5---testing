38773007d96b6f5adcc46ae7501b1168c53d8685
MikkelBruun <mikkelbruunibsen@gmail.com>
2024-12-04 13:28:43 +0100

    apdate

--- rettelser.md
+++ rettelser.md
@@ -1,7 +1,32 @@
 # Rettelser
 
-Opdateret requirements.txt så det inkluderer alle dependencies. Inklusiv xlsxwriter, som teoretisk set er optional, men er nødvendig i dette projekt for at åbne excel filer.
+Rettelser i kode er generelt markeret med `## Rettelse`, hvis der er tale om en enkelt linje:
+
+```python
+if downloaded:
+    finished_dict[name]="yes" ## RETTELSE
+else:
+    finished_dict[name]="no" ## RETTELSE
+```
+
+Hvis der er en blok af rettelse er den afgrænset med `## RETTELSE` og `## /RETTELSE`:
+
+```python
+## RETTELSE
+#finished_data_frame = pl.from_dict(finished_dict)
+_finished_dict = {ID: [key for key in finished_dict], "pdf_downloaded": [finished_dict[key] for key in finished_dict]}
+finished_data_frame = pl.from_dict(_finished_dict)
+## /RETTELSE
+```
+
+## At køre programmet
 
 For at køre kræver programmet en mappe med en input fil "customer_data\GRI_2017_2020.xlsx". Uden denne kører programmet ikke. README nævner at man kan override en række default filstier, men nævner ikke hvad disse er.
 
-Har tilføjet et optional arg til `File_Handler.start_download`: `max_rows=20`. Som udleveret havde programmet en lignende hardcoded grænse, men det kan nu sættes efter behov.
+Jeg har pdateret requirements.txt så det inkluderer alle dependencies. Inklusiv xlsxwriter, som teoretisk set er optional, men er nødvendig i dette projekt for at åbne excel filer.
+
+Til at starte med var der en hardcoded begrænsning på 20 downloads. Nu har jeg tilføjet et optional arg til `File_Handler.start_download`: `max_rows=20`.
+
+## Fundne fejl
+
+Integrationtests `test_meta_yes_in_files` og `test_meta_no_not_in_files` viste at der ikke var sammenhæng mellem hvad der blev skrevet i metafilen, og hvilke downloads var successfulde. I download_thread bliver der tilføjet til to lister, `ID` og `pdf_downloaded` ("yes"/"no"). Problemet var at filens id blev tilføjet ved download start, og "yes/no" blev tilføjet i slutningen (racecondition). Dette blev løst ved at skrive dem samtidig som key/val i en dict.
--- test.py
+++ test.py
@@ -12,14 +12,18 @@ from Polar_File_Handler import FileHandler
 from pathlib import Path
 import polars as pl
 from Polar_File_Handler import FileHandler
+import runpy
+from copy import copy
 
 @dataclass()
 class T_Paths:
-    customer_data:Path = Path("customer_data")
+    customer_data: Path = Path("customer_data")
     def_metadata_file: Path = customer_data / "Metadata2017_2020.xlsx"
     def_input_file: Path = customer_data / "GRI_2017_2020.xlsx"
     test_sheet_file: Path = Path("test_files/test_sheet.xlsx")
     downloaded_files_folder: Path = Path("files")
+
+
 class T_Data:
     working_urls = [
         "http://www.amgen.com/~/media/amgen/full/www-amgen-com/downloads/responsibility-report/amgen-2016-responsibility-highlights-report.pdf",
@@ -28,65 +32,108 @@ class T_Data:
     def get_working_url(): return T_Data.working_urls[0]
     failing_urls = [
         "http://web.ardentec.com/UserFiles/File/2016%20Ardentec%20CSR%20Report.pdf",
-        
+
     ]
     def get_failing_url(): return T_Data.failing_urls[0]
     timeout_url = "http://about.americanexpress.com/csr/docs/Amex-CSR-Report-Full-2016.pdf"
 
 
 class TestUnit:
-    """
-    Test that when a get request fails, the secondary url is tried
-    """
+    def test_controller_set_parameters(self):
+        """
+        Ensure that Controller parameters are set correctly
+        """
+        C = Controller()
+        _destination, _metafile, _inputfile = [
+            "./dest", "./metafile", "./urlFile"]
+        C.set_destination(_destination)
+        C.set_report_file(_metafile)
+        C.set_url_file(_inputfile)
+        assert (
+            [C.destination, C.report_file_name, C.url_file_name] ==
+            [_destination, _metafile, _inputfile]
+        )
+
     def test_download_file_backup(self, tmp_path, monkeypatch: pytest.MonkeyPatch):
         """Make sure a backup url is used when first url fails"""
         fname = "testfile.pdf"
         DL = Downloader.Downloader()
         get_calls = []
-        def mock_get(*args,**kwargs):
+
+        def mock_get(*args, **kwargs):
             get_calls.append(args[0] in T_Data.working_urls)
             return None
         monkeypatch.setattr("Downloader.requests.get", mock_get)
         DL.download(T_Data.get_failing_url(),
                     tmp_path / fname,
                     alt_url=T_Data.get_working_url())
-        assert(get_calls == [False,True])
-        
-    def test_excel_reading(self, tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
-        # values recived by download_thread as triples (BRid, url1, url2)
-        values = {}
-        def mock_download_thread(self, queue: Queue) -> None:
-            while not queue.empty():
-                link, destination, name, alt_link, finished_dict = queue.get()
-                time.sleep(0.1)
-                values[name] = (link,alt_link)
-                queue.task_done()
-        monkeypatch.setattr(FileHandler, "download_thread", mock_download_thread)
-        expected = {f"BR{id}":(f"http://internet.site.BR{id}.com", f"http://internet.backup.BR{id}.com")
-                    for id in range(50100, 50157)}
-        FH = FileHandler()
-        FH.start_download(T_Paths.test_sheet_file,tmp_path / "metafilepath","testfiles",max_rows=-1)
-        del values["BRnum"] # remove header
-        assert(values == expected)
+        assert (get_calls == [False, True])
 
+    def test_download_type_exception(self, monkeypatch: pytest.MonkeyPatch, tmp_path: Path):
+        """
+        Check that an exceptikon is (not) thrown according to response content-type header
+        """
+        def mock_get(*args,**kwargs):
+            url = args[0]
+            class Response_mock:
+                def __init__(self, contentType: str) -> None:
+                    self.headers = {"content-type": contentType}
+            return Response_mock("application/pdf" if url in T_Data.working_urls else "invalid/contenttype")
+        monkeypatch.setattr("Downloader.requests.get", mock_get)
+        DL = Downloader.Downloader()
+        
+        with pytest.raises(Exception) as ex_info:
+            DL.download(T_Data.get_failing_url(),
+                        tmp_path/"arbitrary_filename", None)
+            # raises on bad content-type
+            assert ("Not pdf type" in ex_info.value)
+        ex_raised = False
+        with pytest.raises(Exception) as ex_info:
+            DL.download(T_Data.get_working_url(),
+                        tmp_path/"arbitrary_filename", None)
+            ex_raised = ex_info.value is not None
+        # doesn't raise on correct content type
+        assert(not ex_raised)
+    # TEMP TODO track test filer på hjemme computer
+    # def test_excel_reading(self, tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
+    #     """
+    #     Read a testsheet using FileHandler. mock download_thread to ensure they correct urls are recieved
+    #     """
+    #     # values recived by download_thread as triples (BRid, url1, url2)
+    #     values = {}
+
+    #     def mock_download_thread(self, queue: Queue) -> None:
+    #         """save the input to "values" and simply mark the task as done"""
+    #         while not queue.empty():
+    #             link, destination, name, alt_link, finished_dict = queue.get()
+    #             time.sleep(0.1)
+    #             values[name] = (link,alt_link)
+    #             queue.task_done()
+    #     monkeypatch.setattr(FileHandler, "download_thread", mock_download_thread)
+    #     expected = {f"BR{id}":(f"http://internet.site.BR{id}.com", f"http://internet.backup.BR{id}.com")
+    #                 for id in range(50100, 50157)}
+    #     FH = FileHandler()
+    #     FH.start_download(T_Paths.test_sheet_file,tmp_path / "metafilepath","testfiles",max_rows=-1)
+    #     del values["BRnum"] # remove header
+    #     assert(values == expected)
 
 
 class TestIntegration:
-    """
-    tests ending with _R requires a previous run of the program under regular conditions.
-    Will then cross-check the downloaded files with the meta files 
-    """
     @pytest.fixture(scope="class",)
     def prep_integration(self):
+        """
+        Sets up by running the program and putting the results in a temp folder
+        Reused across all integration tests
+        """
         tmp = Path("./tmp")
-        os.makedirs(tmp,exist_ok=True)
+        os.makedirs(tmp, exist_ok=True)
         os.makedirs(tmp / "customer_data", exist_ok=True)
         C = Controller()
         tmp_path = T_Paths(**{
-            "customer_data" : "customer_data",
+            "customer_data": "customer_data",
             "def_metadata_file": tmp/"customer_data"/"Metadata2017_2020.xlsx",
-            "def_input_file" : tmp/"customer_data" / "GRI_2017_2020.xlsx",
-            "downloaded_files_folder" : tmp / "files",
+            "def_input_file": tmp/"customer_data" / "GRI_2017_2020.xlsx",
+            "downloaded_files_folder": tmp / "files",
         })
         pprint(tmp_path)
         os.makedirs(tmp_path.downloaded_files_folder, exist_ok=True)
@@ -100,6 +147,7 @@ class TestIntegration:
         print("teardown")
 
     def test_download_file(self, tmp_path):
+        """Download a single file"""
         fname = "testfile.pdf"
         DL = Downloader.Downloader()
         DL.download(T_Data.get_working_url(),
@@ -107,30 +155,40 @@ class TestIntegration:
         dirfiles = os.listdir(tmp_path)
         assert (len(dirfiles) == 1)
         assert (dirfiles[0] == fname)
-        
+
     def test_downloaded_nameformat_R(self, prep_integration):
+        """Ensure that all pdfs are named correctly"""
         for file in os.listdir(prep_integration.downloaded_files_folder):
-            assert(re.match(r"^BR\d+\.pdf$",file))
+            assert (re.match(r"^BR\d+\.pdf$", file))
 
-    def test_meta_yes_in_files_R(self, prep_integration):
-        meta_data = pl.read_excel(
-            source=prep_integration.def_metadata_file, 
-            columns=["BRnum", "pdf_downloaded"]
-        )
-        files_folder = set(os.listdir(prep_integration.downloaded_files_folder))
-        yes_set = {f"{id}.pdf" for id, status in meta_data.iter_rows()
-                   if status == "yes"}
-        # intersection of these sets should be identical
-        assert(yes_set.intersection(files_folder) == yes_set)
-
-    def test_meta_no_not_in_files_R(self, prep_integration):
+    def test_meta_yes_in_files(self, prep_integration):
+        """
+        Ensure that each "yes" in the metafile, corresponds to a downloaded file
+        Ensure that each downloaded file corresponds to a "yes" entry in the metafile
+        """
         meta_data = pl.read_excel(
             source=prep_integration.def_metadata_file,
             columns=["BRnum", "pdf_downloaded"]
         )
-        files_folder = set(os.listdir(prep_integration.downloaded_files_folder))
+        files_folder = set(os.listdir(
+            prep_integration.downloaded_files_folder))
+        yes_set = {f"{id}.pdf" for id, status in meta_data.iter_rows()
+                   if status == "yes"}
+        # intersection of these sets should be identical
+        assert (yes_set.intersection(files_folder) == yes_set)
+
+    def test_meta_no_not_in_files(self, prep_integration):
+        """
+        Ensure that "no" entries in the metafile, are not actually present
+        """
+        meta_data = pl.read_excel(
+            source=prep_integration.def_metadata_file,
+            columns=["BRnum", "pdf_downloaded"]
+        )
+        files_folder = set(os.listdir(
+            prep_integration.downloaded_files_folder))
         no_set = {f"{id}.pdf" for id, status in meta_data.iter_rows()
-                   if status == "no"}
+                  if status == "no"}
         # these sets should have no overlap (empty set)
         assert (no_set.intersection(files_folder) == set())
 
@@ -143,5 +201,9 @@ class TestIntegration:
             with open(prep_integration.downloaded_files_folder / file, "rb") as F:
                 startChars.append(F.read(4))
         assert (startChars == [b"%PDF" for _ in range(len(startChars))])
+        
+    
+
+
 if __name__ == "__main__":
     pass
